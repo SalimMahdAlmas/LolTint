90a35ee3df29002e87ba628c8fe1c7f986a32a70
Sahid S Almas <robinaf3@gmail.com>
2015-09-22 15:41:02 +0530

    Update Utils.java

--- Source/java/al/androidfire/loltint/Utils.java
+++ Source/java/al/androidfire/loltint/Utils.java
@@ -1,4 +1,16 @@
-
+/*
+ * Copyright (C) 2015 AndroidFire
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  *
@@ -25,4 +37,30 @@ public class Utils {
         }catch (Exception a) {
             a.printStackTrace();
         }
-        retur
+        return color;
+    }
+    /**
+     * This method make view into bitmap using drawing cache
+     * @param view The view you want to transfer
+     * @return Bitmap based on the view
+     */
+    public static Bitmap transferViewIntoBitmap(View view) {
+        Bitmap result;
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        result = view.getDrawingCache();
+        return result;
+    }
+    /**
+     * Get rect from view
+     * @param view The view from which you want Rect
+     * @return Rect from the view
+     */
+    public static Rect getRectFromView(View view) {
+        Rect rect = new Rect();
+        view.getWindowVisibleDisplayFrame(rect);
+        return rect;
+    }
+
+
+}
