c74ef04c4c462fb30eb9072660061e348fef6436
Sahid S Almas <robinaf3@gmail.com>
2015-09-22 15:35:50 +0530

    Update Color.java

--- Source/java/al/androidfire/loltint/Color.java
+++ Source/java/al/androidfire/loltint/Color.java
@@ -15,7 +15,7 @@ public class Color extends android.graphics.Color {
     public static int COLOR_PURPLE = parseColor("9C27B0");
     public static int COLOR_INDIGO = parseColor("3F51B5");
     public static int COLOR_BLUE = parseColor("2196F3");
-    public static  int COLOR_TEAL = parseColor("009688");
+    public static int COLOR_TEAL = parseColor("009688");
     public static int COLOR_GREEN = parseColor("4CAF50");
     public static int COLOR_YELLOW = parseColor("FFEB3B");
     public static int COLOR_BROWN = parseColor("FFEB3B");
@@ -27,7 +27,12 @@ public class Color extends android.graphics.Color {
     public  static int COLOR_DEEP_PURPLE = parseColor("673AB7");
 
 
-
+    /**
+     * This method make interger based color into argb
+     * 7f00000 into 0xxxxxx code
+     * @param color Color in int you want to convert
+     * @return argb form integer based color
+     */
     public static String convertToARGB(int color) {
         String alpha = Integer.toHexString(android.graphics.Color.alpha(color));
         String red = Integer.toHexString(android.graphics.Color.red(color));
@@ -53,15 +58,11 @@ public class Color extends android.graphics.Color {
         return "#" + alpha + red + green + blue;
     }
 
-    public static String toHex(int color) {
-
-        String ALGORITHM = "0123456789ABCDEF";
-        color = Math.max(0, Math.min(color, 255));
-
-        return String.valueOf(ALGORITHM.charAt((color - color % 16) / 16)
-                + ALGORITHM.charAt(color % 16));
-
-    }
+    /**
+     * Return the darker version depend upon the facor
+     * @param colorZ the color you want to convert
+     * @return Darker version
+     */
 
     public static int toDarker(int colorZ) {
         float[] hsv = new float[3];
@@ -89,81 +90,6 @@ public class Color extends android.graphics.Color {
                 Math.max( (int)(g * factor), 0 ),
                 Math.max( (int)(b * factor), 0 ) );
     }
-
-
-
-
-
-
-    public static int alpha(int color) {
-        return color >>> 24;
-    }
-
-
-    public static int red(int color) {
-        return (color >> 16) & 0xFF;
-    }
-
-    public static int green(int color) {
-        return (color >> 8) & 0xFF;
-    }
-
-    public static int blue(int color) {
-        return color & 0xFF;
-    }
-
-
-    public static int rgb(int red, int green, int blue) {
-        return (0xFF << 24) | (red << 16) | (green << 8) | blue;
-    }
-
-
-    public static int argb(int alpha, int red, int green, int blue) {
-        return (alpha << 24) | (red << 16) | (green << 8) | blue;
-    }
-
-    public static float saturation(int color) {
-        int r = (color >> 16) & 0xFF;
-        int g = (color >> 8) & 0xFF;
-        int b = color & 0xFF;
-
-
-        int V = Math.max(b, Math.max(r, g));
-        int temp = Math.min(b, Math.min(r, g));
-
-        float S;
-
-        if (V == temp) {
-            S = 0;
-        } else {
-            S = (V - temp) / (float) V;
-        }
-
-        return S;
-    }
-
-    public static int parseColor(String a) {
-        String Color = null;
-        if (!a.startsWith("#")) {
-            Color = "#"+a;
-        }
-        else {
-            Color = a;
-        }
-        return android.graphics.Color.parseColor(Color);
-
-    }
-
-    public static float brightness(int color) {
-        int r = (color >> 16) & 0xFF;
-        int g = (color >> 8) & 0xFF;
-        int b = color & 0xFF;
-
-        int V = Math.max(b, Math.max(r, g));
-
-        return (V / 255.f);
-    }
-
 
 
 
